938f42e7a64757469ce871e524c102546afe88ff
yadav <yadavkvijayan@gmail.com>
2026-07-10 00:08:00 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.0.1",
+    author="Yadav K",
+    author_email="yadavkvijayan@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
